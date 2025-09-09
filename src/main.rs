db8bef7be418c80336ff444196e7ed5e3c1b54d1
Philip Zhang <zhangxilai@doublechaintech.com>
2025-09-09 19:39:46 +0800

    🚀adding log

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@ use rand::rngs::OsRng;
 mod generate_address;
 mod kown_address_set;
 use kown_address_set::StringSet;
+use tracing::{info};
+
 fn main() {
     // 1) 随机生成私钥
 
     let mut rng = OsRng;
     let mut data = [0u8; 32];
-    data[0] = 1; // 第一个字节为 1，其余都是 0
-    //rng.fill_bytes(&mut data);
-    println!("1");
+    rng.fill_bytes(&mut data);
+    tracing_subscriber::fmt::init();
+    info!("started");
     let string_set = StringSet::from_file("./address.list").expect("无法加载 address.list 文件");
-    println!("Loaded {} entries", string_set.len());
+    info!("Loaded {} entries", string_set.len());
     let count = 1_000_000_000;
-    let report_count = 1_0_000_000;
-    for i in 0..count {
+    //let report_count = 1_0_000_000;
+    for _ in 0..count {
         let keyinfo = generate_address::generate_address(&data);
         let test_words = vec![keyinfo.p2pkh, keyinfo.p2wpkh];
         for word in test_words {
@@ -23,14 +25,9 @@ fn main() {
                 println!("address {} has key {} 在集合中", word, keyinfo.wif);
             }
         }
-        if i % report_count == 0 {
-            println!("finish {} times", i)
-        }
-        // TODO: 写入文件，而不是全存在内存里
-        // 比如写 CSV / JSON
     }
 
-    println!("finish {} times!", count);
+    info!("finish {} times!", count);
     // let keyinfo = verify_by_address(&data);
 
     // println!("{:#?}", keyinfo);
